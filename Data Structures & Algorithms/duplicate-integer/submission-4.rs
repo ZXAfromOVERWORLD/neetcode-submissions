1e6c983af8443ecdc98effce37294c377d446025
ZXAfromOVERWORLD <167204215+ZXAfromOVERWORLD@users.noreply.github.com>
2026-08-14 12:06:19 +0530

    Add: duplicate-integer - submission-4

---
+++ Data Structures & Algorithms/duplicate-integer/submission-4.rs
@@ -0,0 +1,13 @@
+use std::collections::HashSet;
+impl Solution {
+    pub fn has_duplicate(nums: Vec<i32>) -> bool {
+        let mut set = HashSet::new();
+        for i in nums{
+            if !set.insert(i){
+                return true;
+            }
+            set.insert(i);
+        }
+        return false;
+    }
+}
