638b15813cad2cf5aa30fd26d4a4a0c32b35ae5d
ZXAfromOVERWORLD <167204215+ZXAfromOVERWORLD@users.noreply.github.com>
2026-08-23 23:42:59 +0530

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+       if(s.length() != t.length()){
+        return false;
+       } 
+
+       vector<int> ans(26,0);
+
+       for (int i = 0; i < s.length();i++){
+        ans[s[i] - 'a']++;
+        ans[t[i] - 'a']--;
+       }
+
+       for (int i : ans){
+        if (i != 0){
+            return false;
+        }
+       }
+       return true;
+    }
+};
